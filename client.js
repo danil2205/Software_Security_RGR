777b3d0e9050809b66cca14030767eb1e7f5e7c3
danil2205 <04danil@gmail.com>
2025-01-02 21:43:49 +0200

    added: server response with public key

--- client.js
+++ client.js
@@ -3,13 +3,19 @@
 const net = require('net');
 const crypto = require("crypto");
 
-const clientHello = crypto.randomBytes(20).toString('hex');
+const clientHello = crypto.randomBytes(16).toString('hex');
 
 const client = net.createConnection({ port: 3000 }, () => {
   console.log('Connected to server');
 
-  console.log(`Send to server: ${clientHello}`);
+  console.log(`Send to server "hello": ${clientHello}`);
   client.write(clientHello);
+});
+
+client.on('data', (data) => {
+  const { serverHello, publicKey } = JSON.parse(data.toString());
+  console.log(`Received from server "hello": ${serverHello}`);
+  console.log(`Received server public key:\n${publicKey}`);
 });
 
 client.on('end', () => {
--- server.js
+++ server.js
@@ -1,14 +1,28 @@
 'use strict';
 
 const net = require('net');
+const crypto = require('crypto');
 const PORT = 3000;
+
+const { publicKey, privateKey } = crypto.generateKeyPairSync('rsa', {
+  modulusLength: 2048,
+});
 
 const server = net.createServer((socket) => {
   console.log('Client connected');
 
   socket.on('data', (data) => {
     const clientHello = data.toString();
-    console.log(`Received from client: ${clientHello}`);
+    console.log(`Received from client "hello": ${clientHello}`);
+
+    const serverHello = crypto.randomBytes(16).toString('hex');
+    console.log(`Send to client "hello": ${serverHello}`);
+    socket.write(
+      JSON.stringify({
+        serverHello,
+        publicKey: publicKey.export({ type: 'spki', format: 'pem' }),
+      })
+    );
   });
 
   socket.on('end', () => {
